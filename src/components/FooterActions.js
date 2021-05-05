b8fcc968345a1f573a6150b740a0b08672bc41d7
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-05 12:20:03 +0200

    react icons import

--- src/components/FooterActions.js
+++ src/components/FooterActions.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import {FaListUl, FaCheck, FaPlus, FaTrashAlt} from 'react-icons/fa' /*import of font-awesome icons */
+import { IconContext } from "react-icons";
 import '../styles/FooterActions.css'
+
 
 function FooterActions () {
     return [
-
+        
         <footer className="stretched-footer">
+            <IconContext.Provider value={{ className: 'react-icons' }}>
             <div className="footer-buttons-container">
-                <a href="#" className="button">Liste</a>
-                <a href="#" className="button">Terminée</a>
-                <a href="#" className="button">Ajouter</a>
+                <div className="">
+                    <a href="#" className="button" alt="Liste" ><FaListUl /></a>
+                    <a href="#" className="button" alt="Complétées"><FaCheck /></a>
+                    <a href="#" className="button" alt="Ajouter"><FaPlus /></a>
+                </div>
+                <div>
+                    <a href="#" className="button" alt="Supprimer"><FaTrashAlt /></a>
+                </div>
             </div>
+            </IconContext.Provider>
         </footer>
     ]
 }
--- src/components/List.js
+++ src/components/List.js
@@ -9,15 +9,15 @@ function List () {
             <ul>
                 <li className="list-group">
                     Trouver un super job de développeuse
-                    <button className="button button-checked"><img src={iconChecked} alt="icon-checked" /></button>
+                    <button className="button button-checked"><img src={iconChecked} alt="Icon checked" /></button>
                 </li>
                 <li className="list-group">
                     Ranger ma chambre
-                    <button className=" button button-checked"><img src={iconChecked} alt="icon-checked" /></button>
+                    <button className=" button button-checked"><img src={iconChecked} alt="Icon checked" /></button>
                 </li>
                 <li className="list-group">
                     Faire des câlins aux chats
-                    <button className="button button-checked"><img src={iconChecked} alt="icon-checked" /></button>
+                    <button className="button button-checked"><img src={iconChecked} alt="Icon checked" /></button>
                 </li>
             </ul> 
         </main>
