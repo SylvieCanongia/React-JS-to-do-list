9c800ac19f93c937d8d085d565bbedb5b9e60a1d
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-09 16:38:58 +0200

    home redirection after new task creation

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -6,6 +6,7 @@ class AddTask extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.onAddTask(this.newTask.value);
+        this.props.history.push('/')
     }
 
     render() {
